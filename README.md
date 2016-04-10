8eb5a495717b99e3f5badc40c88fd8943b293bb7
Lucy Kang <lucykang@nyu.edu>
2016-04-10 12:52:44 -0400

    update readme

---
+++ README.md
@@ -0,0 +1,7 @@
+![Alt text](data/vacantunits.png)
+
+## [Vacant Units for Rent in NYC](http://livenlulu.github.io/rentalvacancy/)
+
+Features: Bootstrap, Leaflet, NVD3
+
+Data source: Bureau of the Census 2010 Census PL 94 – 171 files for April 1, 2010, Population Estimates Program supplies various input files for age sex race as of 4/1/2010, ACS survey data (5 year version) has been used for a Summary type file and substitutes on an annual basis the previous decennial SF3 files.
--- js/script2.js
+++ js/script2.js
@@ -220,7 +220,7 @@ nv.addGraph(function() {
 function exampleData() {
   return {
     "title":"Revenue",    //Label the bullet chart
-    "subtitle":"US$, in thousands",   //sub-label for bullet chart
+    "subtitle":"US$",   //sub-label for bullet chart
     "ranges":[150,225,300],  //Minimum, mean and maximum values.
     "measures":[220],    //Value representing current measurement (the thick blue line in the example)
     "markers":[250]      //Place a marker on the chart (the white triangle marker)
